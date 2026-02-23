ba74d17bb66a1920aa7e41d64bae53e7b12ef8bf
binbin407 <107537388@qq.com>
2026-02-23 09:53:36 +0800

    fix(WP01): use PostgreSQL 15-safe zhparser config setup

--- api/migrations/000_extensions.sql
+++ api/migrations/000_extensions.sql
@@ -13,19 +13,5 @@ BEGIN
 END
 $$;
 
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM pg_ts_config_map m
-    JOIN pg_ts_config c ON c.oid = m.mapcfg
-    JOIN pg_ts_parser p ON p.oid = c.cfgparser
-    JOIN pg_ts_token_type(p.prsname) t ON t.tokid = m.maptokentype
-    WHERE c.cfgname = 'chinese_zh'
-      AND t.alias IN ('n', 'v', 'a', 'i', 'e', 'l', 'j', 'h', 'k', 'x')
-  ) THEN
-    ALTER TEXT SEARCH CONFIGURATION chinese_zh
-      ADD MAPPING FOR n, v, a, i, e, l, j, h, k, x WITH simple;
-  END IF;
-END
-$$;
+ALTER TEXT SEARCH CONFIGURATION chinese_zh
+  ADD MAPPING FOR n, v, a, i, e, l, j, h, k, x WITH simple;
